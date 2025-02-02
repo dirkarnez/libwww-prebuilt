0c8f0cf4a03641fb2355f55c26f5e2326ba05894
Dirk Arnez <smalldirkalex@gmail.com>
2025-02-02 12:47:51 +0800

    - update local-build.sh

--- local-build.sh
+++ local-build.sh
@@ -5,7 +5,7 @@ export PATH="$workspace/mingw64:$workspace/mingw64/bin:$workspace/msys64/usr/bin
 g++ --version && \
 cd $1 && \
 autoreconf -ivf && \
-cat ./configure && \
+sed -i "s/RM='\$RM'/RM='\$RM -f'/" ./configure && \
 ./configure --help && \
 ./configure --with-expat=no --enable-static --prefix="$workspace/installation" && \
 make && \
