b8ddbfbaa504734c448425fab22557e392aaca73
Dirk Arnez <smalldirkalex@gmail.com>
2025-02-02 11:47:01 +0800

    - update local-build.sh

--- local-build.sh
+++ local-build.sh
@@ -5,5 +5,5 @@ export PATH="$workspace/mingw64:$workspace/mingw64/bin:$workspace/msys64/usr/bin
 g++ --version && \
 cd $1 && \
 ./configure --help && \
-./configure -static --prefix="$(pwd)/installation" && \
+./configure -static --prefix="$workspace/installation" && \
 make install
